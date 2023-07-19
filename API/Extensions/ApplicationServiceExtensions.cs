b251e26dcb0338b10c34d68798af9fc31bd29d4f
toi.vomanhdung@gmail.com <toi.vomanhdung@gmail.com>
2023-07-19 22:12:41 +0700

    End of section 8

--- API/Extensions/ApplicationServiceExtensions.cs
+++ API/Extensions/ApplicationServiceExtensions.cs
@@ -1,4 +1,5 @@
 using API.Data;
+using API.Data.Repositories;
 using API.Interfaces;
 using API.Services;
 using Microsoft.EntityFrameworkCore;
@@ -15,6 +16,8 @@ namespace API.Extensions
                 opt.UseSqlite(configuration.GetConnectionString("DefaultConnection"));
             });
             services.AddScoped<ITokenService, TokenService>();
+            services.AddScoped<IUserRepository, UserRepository>();
+            services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 
             return services;
         }
