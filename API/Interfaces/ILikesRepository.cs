06f134f6973841c3ab97c9541710aabefcfb0b17
toi.vomanhdung@gmail.com <toi.vomanhdung@gmail.com>
2023-08-22 13:24:23 +0700

    End of section 14 (add Like)

---
+++ API/Interfaces/ILikesRepository.cs
@@ -0,0 +1,14 @@
+using API.DTOs;
+using API.Entities;
+using API.Helpers;
+using API.Helpers.PaginationHelpers;
+
+namespace API.Interfaces
+{
+    public interface ILikesRepository
+    {
+        Task<UserLike> GetUserLike(int sourceUserId, int targetUserId);
+        Task<AppUser> GetUserWithLikes(int userId);
+        Task<PagedList<LikeDto>> GetUserLikes(LikesParams likesParams);
+    }
+}
